1d79eb241fccb0a8c120e9db546278ef3d52e7ce
Mari Sato <msato@db.soc.i.kyoto-u.ac.jp>
2021-01-07 02:13:16 +0900

    20210107

--- ch08/ex02/ftpserver/ftp/respond.go
+++ ch08/ex02/ftpserver/ftp/respond.go
@@ -7,7 +7,6 @@ import (
 
 const (
 	status200 = "200 Command okay."
-	// status226 = "226 Closing data connection. Requested file action successful."
 	status425 = "425 Can't open data connection."
 	status426 = "426 Connection closed; transfer aborted."
 	status501 = "501 Syntax error in parameters or arguments."
