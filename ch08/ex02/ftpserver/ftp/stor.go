546cd9ed4111d8c7a20ec700354c6e0fb803ee46
Mari Sato <msato@db.soc.i.kyoto-u.ac.jp>
2021-01-10 23:57:06 +0900

    20210110

--- ch08/ex02/ftpserver/ftp/stor.go
+++ ch08/ex02/ftpserver/ftp/stor.go
@@ -1,11 +1,26 @@
 package ftp
 
+import (
+	"io"
+	"os"
+)
+
 func (c *Conn) stor(args []string) {
+	if len(args) != 1 {
+		c.respond(status501)
+	}
 	dataConn, err := c.dataConnect()
 	if err != nil {
 		c.respond(status425)
 	}
+	fname := args[0]
+
+	file, err := os.Create(fname)
 	defer dataConn.Close()
+	_, err = io.Copy(dataConn, file)
+	if err != nil {
+		c.respond(status426)
+	}
 
 	c.respond(status200)
 }
